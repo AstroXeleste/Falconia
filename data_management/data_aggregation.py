c1caa681fb84e599282114097bd761890c3175da
aßtrö <astroxeleste@aol.com>
2025-02-02 17:41:47 -0500

    Whats a branch?

--- data_management/data_aggregation.py
+++ data_management/data_aggregation.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
 
-def combine_datasets(list_of_dataframes):
-    
+def combine_datasets(list_of_dataframes) -> pd.DataFrame:
+    df = pd.DataFrame({'Reading'})
+    for dataframe in list_of_dataframes:
+        pd.merge(df, dataframe)
+    return df
